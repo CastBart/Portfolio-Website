74d98fa4c90583b975802d42e7607f8119a68968
CastBart <bartosz.j.zych@gmail.com>
2024-10-01 20:46:30 +0100

    Mobile navbar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AboutMe from "./components/AboutMeSection";
-import { NavBar } from "./components/NavBar";
+import NavBar from "./components/NavBar";
 
 export default function Home(): React.ReactNode {
   return (
--- src/pages/home/components/NavBar.tsx
+++ src/pages/home/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { NavBarItem } from "../../../lib/definitions";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const navItems: NavBarItem[] = [
   { id: "1", href: "#about_me", text: "About me" },
@@ -8,7 +8,7 @@ const navItems: NavBarItem[] = [
   { id: "4", href: "#contact_me", text: "Contact me" },
 ];
 
-export const NavBar = () => {
+export default function NavBar() : React.ReactNode {
   const [nav, setNav] = useState(true);
   function handleNav() {
     setNav(!nav);
@@ -17,12 +17,48 @@ export const NavBar = () => {
     <nav id="nav_bar" className="fixed top-0 w-full p-2">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo */}
-        <div>Logo</div>
+        <div className="p-2 text-sm uppercase">Logo</div>
         {/* Desktop Navibation */}
         <ul className="hidden items-center space-x-10 text-sm uppercase lg:flex ">
           {navItems.map((item) => {
             return (
-              <li className="cursor-pointer rounded-md p-2 transition-shadow duration-300 ease-in-out hover:shadow-sm hover:shadow-white">
+              <li key={item.id} className="cursor-pointer rounded-md p-2 transition-shadow duration-300 ease-in-out hover:shadow-sm hover:shadow-white">
+                <a className="" href={item.href}>
+                  {item.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="hidden lg:block p-2 text-sm uppercase">Mode</div>
+        {/* Hamburger menu */}
+        <div className="lg:hidden z-10" onClick={handleNav}>
+          <div
+            className={`flex flex-col h-8 w-8 ms-center justify-center space-y-1 transition-all duration-300 ease-in-out transform`}
+          >
+            <div
+              className={`w-6 h-0.5 bg-white transition-all duration-200 ease-in-out transform ${
+                !nav ? "rotate-45 translate-y-1" : ""
+              }`}
+            ></div>
+            <div
+              className={`w-6 h-0.5 bg-white transition-opacity duration-200 ease-in-out ${
+                !nav ? "opacity-0" : "opacity-100"
+              }`}
+            ></div>
+            <div
+              className={`w-6 h-0.5 bg-white transition-all duration-200 ease-in-out transform ${
+                !nav ? "-rotate-45 -translate-y-2" : ""
+              }`}
+            ></div>
+          </div>
+        </div>
+      </div>
+        {/* Mobile NavBar */}
+        <ul className={`lg:hidden flex z-0 flex-col fixed pt-9 bg-[#3a3838] w-[60%] h-full justify-start items-center space-y-10 text-sm rounded-l-3xl uppercase ease-in-out duration-500 top-0 ${nav ? '-right-[100%]' : 'right-0'}`}>
+          {navItems.map((item) => {
+            return (
+              <li key={item.id} className="cursor-pointer rounded-md p-2 transition-shadow duration-300 ease-in-out hover:shadow-sm hover:shadow-white">
                 <a className="" href={item.href} id={item.id}>
                   {item.text}
                 </a>
@@ -30,33 +66,6 @@ export const NavBar = () => {
             );
           })}
         </ul>
-        {/* Mobile  */}
-        <div className="hidden lg:block">Mode</div>
-        {/* Hamburger menu */}
-        <div className="lg:hidden" onClick={handleNav}>
-          <div
-            className={`flex flex-col items-center justify-center space-y-1 transition-all duration-300 ease-in-out transform ${
-              !nav ? "" : ""
-            }`}
-          >
-            <div
-              className={`w-6 h-0.5 bg-white transition-all duration-300 ease-in-out transform ${
-                !nav ? "rotate-45 translate-y-1" : ""
-              }`}
-            ></div>
-            <div
-              className={`w-6 h-0.5 bg-white transition-opacity duration-300 ease-in-out ${
-                !nav ? "opacity-0" : "opacity-100"
-              }`}
-            ></div>
-            <div
-              className={`w-6 h-0.5 bg-white transition-all duration-300 ease-in-out transform ${
-                !nav ? "-rotate-45 -translate-y-2" : ""
-              }`}
-            ></div>
-          </div>
-        </div>
-      </div>
     </nav>
   );
 };
