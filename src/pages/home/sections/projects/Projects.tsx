237a6d8704a72c9cf8a6e294d2b63d1a986b65f2
CastBart <bartosz.j.zych@gmail.com>
2024-10-29 08:48:39 +0000

    Fixing issue with projects bleeding through the navbar

--- src/pages/home/sections/projects/Projects.tsx
+++ src/pages/home/sections/projects/Projects.tsx
@@ -57,17 +57,20 @@ export default function Projects(): React.ReactNode {
   const handleOpenDialog = (project: Project) => {
     setSelectedProject(project);
     setIsDialogOpen(true);
-    document.body.classList.add('overflow-hidden'); // Disable page scroll
+    document.body.classList.add("overflow-hidden"); // Disable page scroll
   };
 
   const handleCloseDialog = () => {
     setIsDialogOpen(false);
     setSelectedProject(null);
-    document.body.classList.remove('overflow-hidden'); // Re-enable page scroll
+    document.body.classList.remove("overflow-hidden"); // Re-enable page scroll
   };
 
   return (
-    <section id="projects" className="min-h-screen flex justify-center items-center z-10">
+    <section
+      id="projects"
+      className="min-h-screen flex justify-center items-center z-10 relative"
+    >
       <div className="flex justify-center items-center flex-col">
         <h1 className="text-4xl font-bold p-2 pb-10">Projects</h1>
         <div className="flex items-center space-x-4">
@@ -81,28 +84,33 @@ export default function Projects(): React.ReactNode {
           </button>
 
           {/* Project Cards Container */}
-          <div className=" w-[300px] sm:w-[600px] lg:w-[900px] overflow-hidden">
+          <div className="w-[300px] sm:w-[600px] lg:w-[900px] overflow-hidden">
             <div
-              className="flex transition-transform duration-500  ease-in-out"
+              className="flex transition-transform duration-500 ease-in-out relative"
               style={{
-                transform: `translateX(-${(currentIndex * 100) / visibleCards}%)`,
+                transform: `translateX(-${
+                  (currentIndex * 100) / visibleCards
+                }%)`,
               }}
             >
               {projects.map((project, index) => {
-                // Determine if the project is visible
-                const isVisible = index >= currentIndex && index < currentIndex + visibleCards;
+                const isVisible =
+                  index >= currentIndex && index < currentIndex + visibleCards;
 
                 return (
                   <div
                     key={project.id}
-                    className={`w-[100%] sm:w-[50%] lg:w-[33.33%] flex justify-center flex-shrink-0 transition-opacity duration-500 ease-in-out ${
+                    className={`w-[100%] sm:w-[50%] lg:w-[33.33%] flex justify-center flex-shrink-0 transition-opacity duration-500 ease-in-out relative ${
                       isVisible ? "opacity-100" : "opacity-0"
                     }`}
                     style={{
-                      transitionProperty: "opacity, transform",
+                      zIndex: isVisible ? 10 : 0,
                     }}
                   >
-                    <ProjectCard project={project} onMoreClick={handleOpenDialog} />
+                    <ProjectCard
+                      project={project}
+                      onMoreClick={handleOpenDialog}
+                    />
                   </div>
                 );
               })}
--- src/pages/home/sections/projects/components/ProjectDialog.tsx
+++ src/pages/home/sections/projects/components/ProjectDialog.tsx
@@ -4,7 +4,7 @@ export function ProjectDialog({ isOpen, onClose, project }: ProjectDialogProps) 
   if (!isOpen) return null; // Do not render the dialog if it is closed
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-20">
       <div className="bg-[#242424] p-6 rounded-lg w-[90%] max-w-lg h-[80vh] overflow-y-auto dialog-content">
         {/* Display the project title */}
         <h3 className="text-2xl font-bold mb-4">{project.title}</h3>
